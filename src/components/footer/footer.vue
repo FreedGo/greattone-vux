9d0d136e1661702cffada9b5d9b1b080e0e8759b
Freed <flyxz@126.com>
2017-08-08 18:37:28 +0800

    音乐广场轮播图，主列表完成

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,23 +1,29 @@
 <template>
 		<tabbar >
 			<tabbar-item  selected>
-				<img slot="icon" src="../../assets/demo/icon_nav_button.png">
+				<img slot="icon" src="../../assets/greattone/index.png">
+				<img slot="icon-active" src="../../assets/greattone/index-after.png">
 				<span slot="label">首页</span>
 			</tabbar-item>
 			<tabbar-item  show-dot>
-				<img slot="icon" src="../../assets/demo/icon_nav_msg.png">
+				<img slot="icon" src="../../assets/greattone/huodong.png">
+				<img slot="icon-active" src="../../assets/greattone/huodong-after.png">
 				<span slot="label">音乐活动</span>
 			</tabbar-item>
 			<tabbar-item  link="/component/demo">
-				<img slot="icon" src="../../assets/demo/icon_nav_article.png">
+				<img slot="icon" src="../../assets/greattone/fatie.png">
+				<img slot="icon-active" src="../../assets/greattone/fatie-after.png">
 				<span slot="label">发布</span>
 			</tabbar-item>
 			<tabbar-item   badge="2">
-				<img slot="icon" src="../../assets/demo/icon_nav_cell.png">
+				<img slot="icon" src="../../assets/greattone/message.png">
+				<img slot="icon-active" src="../../assets/greattone/message-after.png">
 				<span slot="label">我的动态</span>
 			</tabbar-item>
 			<tabbar-item badge="2" link="/session/login">
-				<img slot="icon" src="../../assets/demo/icon_nav_cell.png">
+				<img slot="icon" src="../../assets/greattone/center.png">
+				<img slot="icon-active" src="../../assets/greattone/center-after.png">
+
 				<span slot="label">个人中心</span>
 			</tabbar-item>
 		</tabbar>
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -7,8 +7,70 @@
 			placement="left"
 			:drawer-style="{'background-color':'#35495e', width: '200px'}">
 			<view-box ref="viewBox">
-				<heads :tit="titleContent"></heads>
-
+				<div class="search-wrap">搜索</div>
+				<div class="swipe-wrap">
+					<swiper :list="swipeList" :show-desc-mask="false" :auto="true" :loop="true"></swiper>
+				</div>
+				<div class="main-menu">
+					<grid :rows="4">
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-xx.png">
+							<span slot="label">音乐新星</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-dq.png">
+							<span slot="label">斗琴擂台</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-bs.png">
+							<span slot="label">各地比赛</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-hx.png">
+							<span slot="label">网络海选</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-hd.png">
+							<span slot="label">音乐活动</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-mr.png">
+							<span slot="label">名人专区</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-qh.png">
+							<span slot="label">琴行教室</span>
+						</grid-item>
+						<grid-item >
+							<img slot="icon" src="../../assets/greattone/main-pp.png">
+							<span slot="label">乐器品牌</span>
+						</grid-item>
+					</grid>
+				</div>
+				<div class="mian-list">
+					<tab :line-width="1" custom-bar-width="26px">
+						<tab-item selected>
+							<span >音乐广场</span>
+							<img src="../../assets/greattone/sanjiao.png">
+						</tab-item>
+						<tab-item>关注动态</tab-item>
+						<tab-item>热门动态</tab-item>
+					</tab>
+					<div v-transfer-dom>
+						<popup v-model="subGuang" position="top" :show-mask="true" :hide-on-blur="true" >
+							<div class="position-vertical-demo">
+								<checker
+									selected-item-class="demo4-item-selected"
+									disabled-item-class="demo4-item-disabled">
+									<checker-item value="花跟叶" @on-item-click="onItemClick">花跟叶</checker-item>
+									<checker-item value="鸟与树" @on-item-click="onItemClick">鸟与树</checker-item>
+									<checker-item value="我和你" @on-item-click="onItemClick">我和你</checker-item>
+									<checker-item value="全套礼品装" disabled @on-item-click="onItemClick">全套礼品装</checker-item>
+								</checker>
+							</div>
+						</popup>
+					</div>
+				</div>
 				<!--<heads :tit="titleContent"></heads>-->
 				<router-view></router-view>
 			</view-box>
@@ -22,24 +84,82 @@
 	import foot from '@/components/footer/footer.vue'
 	import heads from '@/components/header/header.vue'
 	//	import login from './components/login/login.vue'
-	import { Drawer,ViewBox } from 'vux'
-
+	import { Drawer,ViewBox,Swiper,AjaxPlugin, Grid, GridItem,Tab,TabItem,TransferDom, Popup,Checker,CheckerItem } from 'vux'
+	const url = 'http://m.greattone.net/e/appapi/';
 	export default {
 		name: 'subapp',
+		created(){
+			this.getSwipeList();
+		},
+		directives:{
+			TransferDom
+		},
 		data(){
 			return{
 				msg:'好琴声',
-				titleContent:'好琴声主页'
+				titleContent:'好琴声主页',
+				swipeList:[],//主轮播图列表
+				subGuang:true
 			}
 		},
 		components:{
-			foot,heads,
-			Drawer,
-			ViewBox
-		}
+			foot,heads, Drawer, ViewBox,Swiper,AjaxPlugin, Grid, GridItem,Tab,TabItem, Popup,Checker,CheckerItem
+		},
+		methods:{
+			getSwipeList(){
+				let params = new URLSearchParams();
+				params.append('api','extend/getAdvList');
+				params.append('classid',5);
+
+				this.$http.post(url,params).then(res=>{
+					let obj = {};
+					let _this = this;
+					if (res.data.err_msg == 'success'){
+						res.data.data.forEach(function (item,index) {
+							obj.url = item.url;
+							obj.img = item.pic;
+//							obj.title = item.title;
+							_this.swipeList.push(obj)
+						})
+					}
+				},res=>{
+
+				}).catch(res=>{
+
+				})
+			},
+			onItemClick (value, disabled) {
+				console.log(value, disabled)
+				if (!this.disabled) {
+					this.showPopup = false
+				}
+			}
+		},
+
 	}
 </script>
 
 <style lang="less">
+	#subapp{
+		background-color:#f2f2f5;
+		.main-menu{
+			background-color: #fff;
+			margin-bottom: 10px;
+		}
+
+		.weui-grid__icon{
+			width:40px;
+			height: 40px;
+		}
+	    .weui-grids:before,.weui-grids:after{
+		    border-color:#fff;
+	    }
+		.weui-grid:before,.weui-grid:after{
+			border-color:#fff;
+		}
+		.weui-grid{
+			padding:16px 2px 2px;
+		}
+	}
 
 </style>
