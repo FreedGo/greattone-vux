f93ec1c8e4ad28f2691b031383894aa9842ed724
Freed <flyxz@126.com>
2017-08-10 18:39:50 +0800

    音乐广场列表视频贴，音乐贴，图片贴展示完成

---
+++ src/components/static/data.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Freed on 2017/8/9.
+ * E-mail:flyxz@126.com.
+ * GitHub:FreedGo@github.com.
+ */
+
+let data = {};
+data.apiurl = 'http://test.greattone.net/e/appapi/';//api请求地址
+
+
+export default data;
+
+
+
+
